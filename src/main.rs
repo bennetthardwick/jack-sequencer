fd7d8b020f4f82e778a8dda602d4fd26f99ca0f4
Bennett Hardwick <me@bennetthardwick.com>
2019-10-13 15:54:36 +1000

    Implement quit message

--- src/main.rs
+++ src/main.rs
@@ -202,6 +202,9 @@ fn main() {
                 Message::UpdateSequencer((track, beat, active)) => {
                     state.sequencer[track][beat] = active;
                 }
+                Message::Quit => {
+                    return;
+                },
                 _ => {
                     unimplemented!();
                 }
